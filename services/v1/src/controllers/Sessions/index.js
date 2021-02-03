4b2bb359032fc742a74e9849ba3f31bb8525f8b2
Tyler Ilunga <tylerilunga1@gmail.com>
2021-02-02 18:59:47 -0800

    setting up the Sessions Controller

---
+++ services/v1/src/controllers/Sessions/index.js
@@ -0,0 +1,39 @@
+/**
+ * Sessions Controller module.
+ * @module app.js
+ */
+module.exports = {
+  /**
+   * Determines whether or not a user's session is still active based on their JWT.
+   *
+   * @param {object} req - Express.js Request
+   * @param {object} res - Express.js Response
+   *
+   * @return {number} Network Status number
+   */
+  reviewSession(req, res) {
+    res.send(200);
+  },
+  /**
+   * Creates a new account and a session for a new user.
+   *
+   * @param {object} req - Express.js Request
+   * @param {object} res - Express.js Response
+   *
+   * @return {object} JSON object
+   */
+  signUp(req, res) {
+    res.send(200);
+  },
+  /**
+   * Creates a new session for an existing user.
+   *
+   * @param {object} req - Express.js Request
+   * @param {object} res - Express.js Response
+   *
+   * @return {number} JSON object
+   */
+  logIn(req, res) {
+    res.send(200);
+  },
+};
--- services/v1/src/routes/index.js
+++ services/v1/src/routes/index.js
@@ -4,7 +4,14 @@
  */
 
 const Router = require('express').Router();
+const SessionsController = require('../controllers/Sessions');
 
+/** Service Health Check */
 Router.get('/healthcheck', (req, res) => res.sendStatus(200));
+
+/** API Endpoints related to handling user sessions */
+Router.get('/api/v1/sessions/review', SessionsController.reviewSession);
+Router.post('/api/v1/sessions/signup', SessionsController.signUp);
+Router.post('/api/v1/sessions/login', SessionsController.logIn);
 
 module.exports = Router;
