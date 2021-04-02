24a158f0049034f2a3f3684f6852832e35ea8ed6
Tyler Ilunga <tylerilunga1@gmail.com>
2021-04-02 15:59:26 -0400

    cleaner way of dealing with CORS issues on the BE

--- services/v1/app.js
+++ services/v1/app.js
@@ -4,6 +4,7 @@
  */
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const logger = require('morgan');
 const routes = require('./src/routes');
 const config = require('./src/config');
@@ -13,19 +14,27 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(config.JWT.secret));
+app.use(
+  cors({
+    // Access-Control-Allow-Credentials
+    credentials: true,
+    // Access-Control-Allow-Origin
+    origin: process.env.CLIENT_URL,
+    // Access-Control-Allow-Methods
+    methods: ['GET', 'HEAD', 'OPTIONS', 'POST', 'PUT', 'DELETE'],
+    // Access-Control-Allow-Headers
+    allowedHeaders: [
+      'Access-Control-Allow-Headers',
+      'Origin',
+      'Accept',
+      'X-Requested-With',
+      'Content-Type',
+      'Access-Control-Request-Method',
+      'Access-Control-Request-Headers',
+    ],
+  }),
+);
 app.use(logger('dev'));
-app.use((req, res, next) => {
-  console.log('process.env.CLIENT_URL:', process.env.CLIENT_URL);
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
-  res.setHeader('Access-Control-Allow-Headers', process.env.CLIENT_URL);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
-  );
-  next();
-});
 app.use('/', routes);
 
 module.exports = app;
