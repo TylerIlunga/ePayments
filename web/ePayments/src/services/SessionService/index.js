c6a0b6fb52f3143f1c49b11bef8eed3bb8106b9b
Tyler Ilunga <tylerilunga1@gmail.com>
2021-04-02 14:48:37 -0400

    Setting up Services on the Client and the SessionService

--- web/ePayments/src/services/SessionService/index.js
+++ web/ePayments/src/services/SessionService/index.js
@@ -1,6 +1,70 @@
+import config from '../config';
+
 class SessionService {
   constructor() {
     this.url = process.env.REACT_APP_EPAYMENTS_SESSION_SERVICE_URL;
+    this.networkRequest = config.networkRequest;
+  }
+
+  signUp(userData) {
+    const { email, password } = userData;
+    return new Promise((resolve, reject) => {
+      this.networkRequest(`${this.url}/signup`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+      })
+        .then((res) => {
+          if (res.error) {
+            throw res.error;
+          }
+          console.log('signUp() res.data:', res.data);
+          resolve(res.data);
+        })
+        .catch((error) => {
+          console.log('signUp() error:', error);
+          reject(error);
+        });
+    });
+  }
+
+  logIn(userData) {
+    const { email, password } = userData;
+    return new Promise((resolve, reject) => {
+      this.networkRequest(`${this.url}/login`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+      })
+        .then((res) => {
+          if (res.error) {
+            throw res.error;
+          }
+          console.log('login() res.data:', res.data);
+          resolve(res.data);
+        })
+        .catch((error) => {
+          console.log('login() error:', error);
+          reject(error);
+        });
+    });
+  }
+
+  logOut() {
+    return new Promise((resolve, reject) => {
+      this.networkRequest(`${this.url}/logout`)
+        .then((res) => {
+          if (res.error) {
+            throw res.error;
+          }
+          console.log('logout() res.data:', res.data);
+          resolve(res.data);
+        })
+        .catch((error) => {
+          console.log('logout() error:', error);
+          reject(error);
+        });
+    });
   }
 }
 
---
+++ web/ePayments/src/services/config.js
@@ -0,0 +1,32 @@
+const config = {
+  networkRequest(url, reqOptions = {}, resType = 'json') {
+    return fetch(url, reqOptions)
+      .then((res) => {
+        if (!res.ok) {
+          return {
+            error: `Invalid Request to ${res.url} with status code ${res.status}`,
+          };
+        }
+        switch (resType) {
+          case 'arrayBuffer':
+            return res.arrayBuffer();
+          case 'blob':
+            return res.blob();
+          case 'formData':
+            return res.formData();
+          case 'text':
+            return res.text();
+          default:
+            return res.json();
+        }
+      })
+      .then((resolvedRes) => {
+        return { data: resolvedRes };
+      })
+      .catch((error) => {
+        return { error };
+      });
+  },
+};
+
+export default config;
